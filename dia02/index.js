014aa74fc05f042272a8b5ebe0076ec0a2ae3ac9
Fernando Vaz <fernandovazz@gmail.com>
2022-08-29 22:17:58 -0300

    criei dia02

---
+++ dia02/index.js
@@ -0,0 +1,23 @@
+function operadoresDeComparacao(){
+    var numero01 = document.getElementById (elementId 'numero01').value;
+    numero01.innerText = "O resultado é" + numero01;
+}
+
+
+/* 
+function operadores() {
+    // =
+
+    var numero1 = 1
+    var numero2 = 2
+
+    // Atribuição de Adição encurtada
+    numero2 +=
+
+}
+
+function operadoresDeComparacao() {
+    // == compara se são iguais
+    var condicao1 = 2 == 2; // Retorna um true ou false, nesse caso retorna true
+}
+*/
